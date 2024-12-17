76f5503dec3c0e36db6580be6629becf66e0d0da
jackhu <jackhu.521.rose@gmail.com>
2024-12-17 16:18:09 +0800

    try to fix version

--- kernel/src/lang_items.rs
+++ kernel/src/lang_items.rs
@@ -11,10 +11,10 @@ fn panic(info: &PanicInfo) -> ! {
             "Panicked at {}:{} {}",
             location.file(),
             location.line(),
-            info.message().unwrap()
+            info.message().as_str().unwrap()
         );
     } else {
-        error!("[kernel] Panicked: {}", info.message().unwrap());
+        error!("[kernel] Panicked: {}", info.message().as_str().unwrap());
     }
     error!("rel4_kernel: PANICED");
     shutdown()
